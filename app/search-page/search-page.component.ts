0f40175a4b2437954c859f6a29970788ddced74b
bolettejurik <baj@kb.dk>
2022-12-06 10:09:37 +0100

    end-user-agreement igen

--- app/search-page/search-page.component.ts
+++ app/search-page/search-page.component.ts
@@ -1,11 +1,21 @@
+// @ts-ignore
 import { Component } from '@angular/core';
 import { SearchPageComponent as BaseComponent } from '../../../../app/search-page/search-page.component';
+import { SEARCH_CONFIG_SERVICE } from '../../../../app/my-dspace-page/my-dspace-page.component';
+import { SearchConfigurationService } from '../../../../app/core/shared/search/search-configuration.service';
 
+// @ts-ignore
 @Component({
   selector: 'ds-search-page',
   // styleUrls: ['./search-page.component.scss'],
   // templateUrl: './search-page.component.html'
-  templateUrl: '../../../../app/search-page/search-page.component.html'
+  templateUrl: '../../../../app/search-page/search-page.component.html',
+  providers: [
+    {
+      provide: SEARCH_CONFIG_SERVICE,
+      useClass: SearchConfigurationService
+    }
+  ]
 })
 
 /**
@@ -13,4 +23,3 @@ import { SearchPageComponent as BaseComponent } from '../../../../app/search-pag
  * It renders search results depending on the current search options
  */
 export class SearchPageComponent extends BaseComponent {}
-

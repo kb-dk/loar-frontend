4a3e47832c6bbac21e345471a89df901891fe9a5
bolettejurik <baj@kb.dk>
2023-06-02 09:26:01 +0200

    about page added

---
+++ src/app/info/about/about-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+/**
+ * Represents the about page
+ */
+@Component({
+  selector: 'ds-about',
+  styleUrls: ['about-page.component.scss'],
+  templateUrl: 'about-page.component.html',
+})
+export class AboutPageComponent {
+}
---
+++ src/app/info/info-routing.module.ts
@@ -0,0 +1,63 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { I18nBreadcrumbResolver } from '../core/breadcrumbs/i18n-breadcrumb.resolver';
+import { ABOUT_PATH ,PRIVACY_PATH, END_USER_AGREEMENT_PATH, FEEDBACK_PATH } from './info-routing-paths';
+import { AboutPageComponent } from './about/about-page.component';
+import { ThemedEndUserAgreementComponent } from './end-user-agreement/themed-end-user-agreement.component';
+import { ThemedPrivacyComponent } from './privacy/themed-privacy.component';
+import { ThemedFeedbackComponent } from './feedback/themed-feedback.component';
+import { FeedbackGuard } from '../core/feedback/feedback.guard';
+import { environment } from '../../environments/environment';
+
+
+const imports = [
+  RouterModule.forChild([
+    {
+      path: ABOUT_PATH,
+      component: AboutPageComponent,
+      resolve: { breadcrumb: I18nBreadcrumbResolver },
+      data: { title: 'info.about', breadcrumbKey: 'info.about' },
+      canActivate: [FeedbackGuard]
+    },
+    {
+      path: FEEDBACK_PATH,
+      component: ThemedFeedbackComponent,
+      resolve: { breadcrumb: I18nBreadcrumbResolver },
+      data: { title: 'info.feedback.title', breadcrumbKey: 'info.feedback' },
+    }
+  ])
+];
+
+  if (environment.info.enableEndUserAgreement) {
+    imports.push(
+      RouterModule.forChild([
+        {
+          path: END_USER_AGREEMENT_PATH,
+          component: ThemedEndUserAgreementComponent,
+          resolve: { breadcrumb: I18nBreadcrumbResolver },
+          data: { title: 'info.end-user-agreement.title', breadcrumbKey: 'info.end-user-agreement' }
+        }
+      ]));
+  }
+  if (environment.info.enablePrivacyStatement) {
+    imports.push(
+      RouterModule.forChild([
+        {
+          path: PRIVACY_PATH,
+          component: ThemedPrivacyComponent,
+          resolve: { breadcrumb: I18nBreadcrumbResolver },
+          data: { title: 'info.privacy.title', breadcrumbKey: 'info.privacy' }
+        }
+      ]));
+  }
+
+@NgModule({
+  imports: [
+    ...imports
+  ]
+})
+/**
+ * Module for navigating to components within the info module
+ */
+export class InfoRoutingModule {
+}
